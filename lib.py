9eac6bcb584d73fa118ba594272519700b705f24
OlegNiedz <oleg.niedz@gmail.com>
2024-02-25 22:31:59 +0100

    7

--- lib.py
+++ lib.py
@@ -8,13 +8,15 @@ def input_error(func):  # Dekorator @input_error
         try:
             return func(*args, **kwargs)
         except ValueError:
-            return "Give me name and phone please."
+            print("Value error.")
         except KeyError:
-            return "No such name found"
+            print("No such name found")
         except IndexError:
-            return "Not found"
+            print("Not found")
+        except TypeError:
+            print("Not found")
         except Exception as e:
-            return f"Error: {e}"
+            print(f"Error: {e}")
 
     return inner
 
@@ -37,15 +39,12 @@ class Phones(UserList):
     def __phone_exist__(self, phone: str):
         return phone in self.data
 
-    #@input_error
     def __normalize_phone__(self, phone: str):
         ptrn = r"[\d\+]+"
         phone = "".join(re.findall(ptrn, phone))
         phone.strip()
-        if len(phone) == 10:
-            return phone
-        else:
-            raise ValueError("Invalid number! (must be 10 digits)")
+
+        #     raise ValueError("Invalid number! (must be 10 digits)")
 
     @input_error
     def find_phone(self, phone: str):
@@ -53,50 +52,71 @@ class Phones(UserList):
         if self.__phone_exist__(phone):
             return self.data[self.data.index(phone)]
         else:
-            raise IndexError
+            raise(IndexError)
 
+    @input_error
     def set_phone(self, phone: str):
-        phone = self.__normalize_phone__(phone)
-        if not self.__phone_exist__(phone):
-            self.data.append(self.__normalize_phone__(phone))
-        else:
-            raise ValueError("Phone exist!")
 
+        phone = self.__normalize_phone__(phone)
+        if len(phone) != 10 or self.__phone_exist__(phone):
+            raise(ValueError)
+        else:
+            self.data.append(phone)
+
+    @input_error
     def edit_phone(self, phone:str, new_phone=""):
         phone = self.__normalize_phone__(phone)
         if self.__phone_exist__(phone):
             new_phone = self.__normalize_phone__(new_phone)
-            if new_phone and not self.__phone_exist__(new_phone):
-                self.data[self.data.index(phone)] = new_phone
+            if len(new_phone) != 10 or self.__phone_exist__(new_phone):
+                raise(ValueError)
             else:
-                self.data.remove(phone)
-        else:
-            raise ValueError("Phone Not exist!")
+                self.data[self.data.index(phone)] = new_phone
 
+    @input_error
+    def delete_phone(self,phone):
+        if self.__phone_exist__(phone):
+            self.data.remove(phone)
+        else:
+            raise(ValueError)
+
+    @input_error
     def get_phones(self):
-        return self.data
+        if self.data==():
+            print("Phones is empty!")
+        else: 
+            return self.data
 
 
 class Birthday(Field):
     def __init__(self, birthday=None):
         self._birthday = birthday
 
+    
     @property
     def birthday(self):
         return self._birthday
 
+    
     @birthday.setter
     def birthday(self, value):
         try:
-            self._birthday = dtdt.strptime(value, "%d-%m-%Y").date()
-        except ValueError:
-            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+            self._birthday = dtdt.strptime(value, "%d.%m.%Y").date()
+        except:
+            print("Invalid date format. Use DD.MM.YYYY")
 
+    
     @property
     def next_birthday(self) -> dtdt.date:
         today = dtdt.today()
         year = (today.year + 1 if self._birthday.month < today.month else today.year)
         return dtdt(year=year, month=self._birthday.month, day=self._birthday.day).date()
+    
+    def __str__(self):
+        return f"{dtdt.strftime(self.birthday,"%d.%m.%Y")}"
+
+    def __repr__(self) -> str:
+        return f"{dtdt.strftime(self.birthday,"%d.%m.%Y")}"
 
 
 class Record:
@@ -112,25 +132,30 @@ class Record:
         else:
             raise ValueError
 
-    def show_birthday(self):
-        print(f"{self.name:25} birthday: {self.birthday:15}")
+    @input_error
+    def show_birthday(self,added_str=None):
+        print(f"{self.name.value:15} birthday: {self.birthday}",end=added_str)
 
     def __str__(self):
-        return f"Contact name: {self.name.value:25}, phones: {'; '.join(p for p in self.phones)}"
+        return f"Contact: {self.name.value:15}, phones: {'; \n'.join(p for p in self.phones)}"
 
     def __repr__(self) -> str:
-        return f"Contact name: {self.name.value:25}, phones: {'; '.join(p for p in self.phones)}"
+        return f"Contact: {self.name.value:15}, phones: {'; \n'.join(p for p in self.phones)}"
 
 
 class AddressBook(UserDict):
+
     @input_error
     def set_record(self, record: Record):
 
         if record.name.value in self.data:
-            raise (ValueError(f"Contact '{record.name.value}' exist!")
+            print(f"Contact '{record.name.value}' exist!")
+            raise(ValueError)
         else:
             self.data[record.name.value] = record
-
+            print(f"{record} added")
+    
+    @input_error
     def get_record(self, record_name: str) -> Record:
         if record_name in self.data:
             return self.data.get(record_name)
@@ -139,7 +164,6 @@ class AddressBook(UserDict):
             return None
 
     def show_all(self):
-        print(self)
         for record_name in self.data:
             print(self.data.get(record_name))
 
--- task1.py
+++ task1.py
@@ -6,12 +6,13 @@ def main():
         "hello": "hello (cmd)",
         "add": "create a new contact",
         "change": "change the contact",
-        "phone": "change the contacts phone",
         "show": "show contacts phone ",
         "all": "show all contacts",
-        "add-birthday": 'format "DD.MM.YYYY"',
-        "show-birthday": "show contacts birthday",
-        "birthdays": "show birthday persons (in this week)",
+        "add-ph": "add phone to contact",
+        "edit-ph": "change the contacts phone",
+        "add-bd": 'format "DD.MM.YYYY"',
+        "show-bd": "show contacts birthday",
+        "bdays": "show birthday persons (in this week)",
         "close": "close aplication",
         "exit": "exit",
     }
@@ -20,16 +21,13 @@ def main():
         msg_promt: str,
         variants: tuple = (),
         msg_err="",
-        ignore_caps=False,
-        allow_empty=False,
-    ):
+        ignore_caps=False
+        ):
         while True:
             answer = input(msg_promt)
             if ignore_caps:
                 answer = answer.lower()
-            if (answer and (not variants or answer in variants)) or (
-                allow_empty and not answer
-            ):
+            if answer and not variants or answer in variants:
                 break
             else:
                 print(msg_err)
@@ -51,65 +49,82 @@ def main():
                 print("\nHow can I help you?\n")
 
             case "add":
-                contact_name = promt(msg_promt="Enter contacts Name: ")
+                contact_name = promt("Enter contacts Name: ",ignore_caps=True)
                 if contact_name:
                     book.set_record(Record(contact_name))
 
             case "change":
                 contact_name = promt(
-                    "Enter Name: ", tuple(book.data.keys()), "Name incorrect!"
+                    "Enter Name: ",
+                    tuple(book.data.keys()),
+                    "Name incorrect!",
+                    ignore_caps=True,
                 )
                 contact = book.get_record(contact_name)
                 new_contact_name = promt("Enter new Name: ")
-                if not (new_contact_name in book.data):
+                if not (new_contact_name in book.data.keys()):
                     contact.edit_name(new_contact_name)
 
-            case "phone":
-                contact_name = promt(
-                    "Enter Name: ", tuple(book.data.keys()), "Name incorrect!"
-                )
-                contact = book.get_record(contact_name)
-                old_phone = promt(
-                    "Enter existing phone (ENTER for Add New): ",
-                    contact.phones,
-                    "Phone Not found!",
-                    allow_empty=True,
-                )
-                new_phone = promt(
-                    f"Enter new phone (ENTER for Delete {old_phone}): ",
-                    allow_empty=True,
-                )
-                if old_phone:
-                    contact.phones.edit_phone(old_phone, new_phone)
+            case "add-ph": 
+                if book.data:                    
+                    contact_name = promt("Enter Name: ", tuple(book.data.keys()), "Name incorrect!",ignore_caps=True)
+                    new_phone = promt("Enter new phone: ")
+                    book.get_record(contact_name).phones.set_phone(new_phone)
+
+            case "edit-ph":
+                if book.data:
+                    contact_name = promt(
+                        "Enter Name: ",
+                        tuple(book.data.keys()),
+                        "Name incorrect!",
+                        ignore_caps=True,
+                    )
+                    contact = book.get_record(contact_name)
+                    if contact.phones:
+                        old_phone = promt(
+                            "Enter existing phone: ",
+                            contact.phones,
+                            "Phone Not found!"
+                        )
+
+                        new_phone = promt("Enter new phone: ")
+                        contact.phones.edit_phone(old_phone, new_phone)
                 else:
-                    contact.phones.set_phone(new_phone)
+                    print(f"Adres book is empty!")
 
             case "show":
                 contact_name = promt(
-                    "Enter Name: ", tuple(book.data.keys()), "Name incorrect!"
+                    "Enter Name: ", tuple(book.data.keys()), "Name incorrect!",ignore_caps=True
                 )
                 print(book.get_record(contact_name))
 
             case "all":
                 book.show_all()
 
-            case "add-birthday":
+            case "add-bd":
                 contact_name = promt(
-                    "Enter Name: ", tuple(book.data.keys()), "Name incorrect!"
+                    "Enter Name: ",
+                    tuple(book.data.keys()),
+                    "Name incorrect!",
+                    ignore_caps=True,
                 )
                 contact = book.get_record(contact_name)
-                contact.birthday = Birthday(promt("Enter birthday DD.MM.YYYY: "))
+                contact.birthday.birthday = promt("Enter birthday DD.MM.YYYY: ")
+                contact.show_birthday()
 
-            case "show-birthday":
+            case "show-bd":
                 contact_name = promt(
-                    "Enter Name: ", tuple(book.data.keys()), "Name incorrect!"
+                    "Enter Name: ",
+                    tuple(book.data.keys()),
+                    "Name incorrect!",
+                    ignore_caps=True,
                 )
-                contact = book.get_record(contact_name).show_birthday()
+                book.get_record(contact_name).show_birthday()
 
             case "birthdays":
 
-                for record in book.data:
-                    record = book.get_record("")
+                for record in book.data.keys():
+                    record = book.get_record(record)
                     congrat_date = record.birthday.next_birthday
                     if 0 <= (congrat_date - dtdt.today().date()).days < 7:
                         match congrat_date.weekday():
@@ -117,6 +132,8 @@ def main():
                                 congrat_date = congrat_date.__add__(timedelta(days=2))
                             case 6:
                                 congrat_date = congrat_date.__add__(timedelta(days=1))
+                        added_str = f"  Congratulation: {dtdt.strftime(congrat_date,'%d.%m.%Y')} \n"
+                        contact.show_birthday(added_str)
 
             case "close" | "exit":
 
